21b5df64e15abbe11f0e84afc679c5d2586bca39
Vincent Spallek <vincent.spallek@gmail.com>
2020-04-19 20:30:32 +0200

    Add S3 sync script

---
+++ sync-s3.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+aws s3 sync ./dist s3://${AWS_BUCKET}/ --profile s3-sync-action --acl public-read --follow-symlinks --delete
